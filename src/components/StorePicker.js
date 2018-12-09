fba2be6b6bb480406575c6b1548195b106df7ec6
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-12-09 21:17:56 +0530

    enabled emmet for jsx in vscode and jsx in component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import  React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Store Picker here!</p>
+        return (
+            <Fragment>
+            <form action="" className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder=""Store Name/>
+                    <button type="submit">Visit Store &rarr;</button>
+            </form>
+            </Fragment>   
+        )    
     }
 }
-
 export default StorePicker;
