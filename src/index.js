bbc40677a349aa3d3fb745b1fa8e5ef2d01a8126
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-12-12 18:05:50 +0530

    sample data for component and unneeded files deletion

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
-import * as serviceWorker from './serviceWorker';
-
-
-render(<StorePicker />, document.getElementById('root'));
 
 
 
+render(<App />, document.getElementById('main'));
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
