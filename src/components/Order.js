38b483558717414c73d04eb0707cdc2f988e3777
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-12-19 14:10:05 +0530

    saving the order state data with localstorage, it saves the ordered prdts data in local storage so that when refreshed or go back the order is still there

--- src/components/Order.js
+++ src/components/Order.js
@@ -5,7 +5,8 @@ class Order extends React.Component {
     renderOrder = (key) => {
         const fish = this.props.fishes[key];
         const count = this.props.order[key];
-        const isAvailable = fish.status === 'available'
+        const isAvailable = fish && fish.status === 'available' //check if there's a fish and its status available
+        if(!fish) return null; //make sure the fish is loaded before we continue, dis was put when debugging local storage err
         if(!isAvailable) {
             return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available.</li>
         }
